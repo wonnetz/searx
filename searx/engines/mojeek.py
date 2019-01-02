1ed6b62b10a95951b3866a8023c5eddb63331135
wonnetz <wonnetz2@gmail.com>
2019-01-01 21:50:28 -0500

    Added mojeek.py

---
+++ searx/engines/mojeek.py
@@ -0,0 +1,15 @@
+"""
+ DuckDuckGo (Web)
+
+ @website     https://duckduckgo.com/
+ @provide-api yes (https://duckduckgo.com/api),
+              but not all results from search-site
+
+ @using-api   no
+ @results     HTML (using search portal)
+ @stable      no (HTML can change)
+ @parse       url, title, content
+
+ @todo        rewrite to api
+"""
+
